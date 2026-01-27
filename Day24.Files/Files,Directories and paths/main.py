baa561412d56ad906f52e39bbe7c61086252f310
Dylan Okomol <etyangdylanokomol@gmail.com>
2026-01-27 15:26:34 +0300

    Day 24 File paths and Directories, Incomplete

---
+++ Day24.Files/Files,Directories and paths/main.py
@@ -0,0 +1,23 @@
+#Files can be open read and written using python.
+#You do this by using the open() function, .read() and .write() methods.
+
+my_file = open("my_file.txt")  #The file can be stored in a variable.
+contents = my_file.read()  #My file is an object and the .read() method retrieves the contents.
+print(contents)
+my_file.close() #We manually close down the file to save computer resources
+
+#We use the with and as keyword to open and manage the file. We no longer have to close the file manually.
+with open("my_file.txt") as file:
+    contents = file.read()
+    print(contents)
+
+#File modes:
+#"r" basically just means read only.
+#"w" sets it to write, so you can write in the file.This deletes everything in the file and replaces.
+#"a" is the append mode. It appends text or whatever, to the file.
+
+#You can create a new file.
+with open("/Users/etyan/Desktop/my_new_file.txt",) as file1:
+    contents = file1.read()
+    print(contents)
+
